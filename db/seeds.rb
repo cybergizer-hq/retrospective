2f93c0f4434114c29c4004e2ef49255a15026106
Ekaterina Egorova <admin.karima@gmail.com>
2021-01-15 11:51:31 +0300

    avoid duplicate records with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,11 @@ Team.create(name: 'Wolves', user_ids: [1, 2, 3, 4, 5]) unless Team.where(name: '
 Team.create(name: 'Tigers', user_ids: [1, 5]) unless Team.where(name: 'Tigers').exists?
 Team.create(name: 'Eagles', user_ids: [2, 3, 4]) unless Team.where(name: 'Eagles').exists?
 
-Board.create([
-               { title: 'TestUser1_RetroBoard' },
-               { title: 'TestUser2_RetroBoard' },
-               { title: 'TestUser3_RetroBoard' },
-               { title: 'TestUser4_RetroBoard' },
-               { title: 'TestUser5_RetroBoard' }
-             ])
+Board.create(title: 'TestUser1_RetroBoard') unless Board.where(title: 'TestUser1_RetroBoard').exists?
+Board.create(title: 'TestUser2_RetroBoard') unless Board.where(title: 'TestUser2_RetroBoard').exists?
+Board.create(title: 'TestUser3_RetroBoard') unless Board.where(title: 'TestUser3_RetroBoard').exists?
+Board.create(title: 'TestUser4_RetroBoard') unless Board.where(title: 'TestUser4_RetroBoard').exists?
+Board.create(title: 'TestUser5_RetroBoard') unless Board.where(title: 'TestUser5_RetroBoard').exists?
 
 Membership.create([
                     { user_id: 1, board_id: 1, role: 'creator', ready: false },
@@ -77,19 +75,15 @@ PermissionsUser.create([
                            board_id: 1 }
                        ])
 
-Card.create([
-              { kind: 'mad', body: 'user1 is very mad', author_id: 1, board_id: 1 },
-              { kind: 'sad', body: 'user1 is very sad', author_id: 1, board_id: 1 },
-              { kind: 'glad', body: 'user1 is very glad #1', author_id: 1, board_id: 1 },
-              { kind: 'glad', body: 'user1 is very glad #2', author_id: 1, board_id: 1 },
-              { kind: 'sad', body: 'user2 is very sad', author_id: 2, board_id: 1 },
-              { kind: 'mad', body: 'user3 is very mad', author_id: 3, board_id: 1 },
-              { kind: 'mad', body: 'user4 is very mad', author_id: 4, board_id: 1 },
-              { kind: 'mad', body: 'user5 is very mad', author_id: 5, board_id: 1 }
-            ])
+Card.create(kind: 'mad', body: 'user1 is very mad', author_id: 1, board_id: 1) unless Card.where(kind: 'mad', author_id: 1, board_id: 1).exists?
+Card.create(kind: 'sad', body: 'user1 is very sad', author_id: 1, board_id: 1) unless Card.where(kind: 'sad', author_id: 1, board_id: 1).exists?
+Card.create(kind: 'glad', body: 'user1 is very glad #1', author_id: 1, board_id: 1) unless Card.where(kind: 'glad', body: 'user1 is very glad #1', author_id: 1, board_id: 1).exists?
+Card.create(kind: 'glad', body: 'user1 is very glad #2', author_id: 1, board_id: 1) unless Card.where(kind: 'glad', body: 'user1 is very glad #2', author_id: 1, board_id: 1).exists?
+Card.create(kind: 'sad', body: 'user2 is very sad', author_id: 2, board_id: 1) unless Card.where(kind: 'sad', author_id: 2, board_id: 1).exists?
+Card.create(kind: 'mad', body: 'user3 is very mad', author_id: 3, board_id: 1) unless Card.where(kind: 'mad', author_id: 3, board_id: 1).exists?
+Card.create(kind: 'mad', body: 'user4 is very mad', author_id: 4, board_id: 1) unless Card.where(kind: 'mad', author_id: 4, board_id: 1).exists?
+Card.create(kind: 'mad', body: 'user5 is very mad', author_id: 5, board_id: 1) unless Card.where(kind: 'mad', author_id: 5, board_id: 1).exists?
 
-ActionItem.create([
-                    { body: 'issue should be fixed', board_id: 1, author_id: 1 },
-                    { body: 'meetings should be held', board_id: 1, author_id: 1 },
-                    { body: 'actions should be taken', board_id: 1, author_id: 1 }
-                  ])
+ActionItem.create(body: 'issue should be fixed', board_id: 1, author_id: 1) unless ActionItem.where(body: 'issue should be fixed', board_id: 1, author_id: 1).exists?
+ActionItem.create(body: 'meetings should be held', board_id: 1, author_id: 1) unless ActionItem.where(body: 'meetings should be held', board_id: 1, author_id: 1).exists?
+ActionItem.create(body: 'actions should be taken', board_id: 1, author_id: 1) unless ActionItem.where(body: 'actions should be taken', board_id: 1, author_id: 1).exists?
